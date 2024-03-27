f83fe485bd78dbc87814cf333834b552b848bcb6
Sunjae Lee <sunjae1294@gmail.com>
2024-03-27 17:00:42 +0900

    update app icon

--- MobileGPT_app/app/src/main/java/com/example/MobileGPT/MobileGPTGlobal.java
+++ MobileGPT_app/app/src/main/java/com/example/MobileGPT/MobileGPTGlobal.java
@@ -1,8 +1,8 @@
 package com.example.MobileGPT;
 
 public class MobileGPTGlobal {
-    public static final String HOST_IP = "";
-    public static final int HOST_PORT = ;
+    public static final String HOST_IP = "192.168.0.124";
+    public static final int HOST_PORT = 12345;
     public static final String STRING_ACTION = "com.example.MobileGPT.STRING_ACTION";
     public static final String INSTRUCTION_EXTRA = "com.example.MobileGPT.INSTRUCTION_EXTRA";
     public static final String APP_NAME_EXTRA = "com.example.MobileGPT.APP_NAME_EXTRA";
